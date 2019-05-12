7716e81243607a4cf37d918f8f9ca3ff46608c39
Julian Kalinowski <julakali@gmail.com>
2019-05-12 13:52:43 +0200

    independent bin image

---
+++ build.sh
@@ -0,0 +1,21 @@
+set -x
+
+docker build -t julakali/mono-build-base base/
+docker build -t julakali/sonarr-source sonarr/
+docker build -t julakali/jackett-source jackett/
+
+docker create --name sonarr julakali/sonarr-source 
+
+rm -r sonarr-bin/opt/Sonarr
+docker cp sonarr:/opt/Sonarr sonarr-bin/opt/
+
+
+docker build -t julakali/sonarr-bin sonarr-bin/
+#docker rm sonarr
+#docker rm jackett
+
+#docker run -d --name sonarr -p 8989:8989 -v /mnt/samba/Serien:/Serien sonarr:latest
+#docker run -d --name jackett -p 9117:9117 jackett
+
+#docker run --name sonarr sonarrimg
+#docker cp sonarr:/src/Sonarr .
